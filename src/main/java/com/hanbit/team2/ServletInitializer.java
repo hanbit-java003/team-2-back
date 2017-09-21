9c718167e21b1b231b884617c5eafdb986799edc
ijy <sereinn@naver.com>
2017-09-21 15:16:36 +0900

    temp

---
+++ src/main/java/com/hanbit/team2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hanbit.team2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Team2Application.class);
+	}
+
+}
